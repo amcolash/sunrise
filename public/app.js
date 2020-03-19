e9bf9c1bd8b01433716ffd4fa38ea437953db225
Andrew McOlash <amcolash@gmail.com>
2020-03-19 00:17:30 -0700

    update ui based on settings

--- public/app.js
+++ public/app.js
@@ -1,14 +1,32 @@
 let ENDPOINT;
 
 const toggle = document.getElementById('toggle');
+const time = document.getElementById('time');
+const duration = document.getElementById('duration');
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
 
 window.onload = function() {
   ENDPOINT = window.location.protocol + '//' + window.location.hostname + ':8001';
+
+  axios.get(`${ENDPOINT}/settings`).then(res => {
+    updateUI(res.data);
+  });
 
   toggle.onclick = () => {
     axios.post(`${ENDPOINT}/lights_off`);
   };
 };
+
+function updateUI(settings) {
+  time.value = settings.time;
+
+  // Clamp duration to acceptable value
+  duration.value = Math.max(settings.duration / 1000 / 60, 5);
+
+  checkboxes.forEach((c, i) => {
+    c.checked = settings.days[i];
+  });
+}
 
 function setIntervalImmediately(func, interval) {
   func();
