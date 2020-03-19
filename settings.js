80f204a08a1f8897c2b4bf646f500236a27865d1
Andrew McOlash <amcolash@gmail.com>
2020-03-19 00:09:24 -0700

    add time / days to settings

--- settings.js
+++ settings.js
@@ -5,6 +5,10 @@ function initSettings() {
   if (!settings.has('interval')) settings.set('interval', 1000);
   if (!settings.has('duration')) settings.set('duration', 30 * 60 * 1000);
   if (!settings.has('max')) settings.set('max', 255);
+  if (!settings.has('time')) settings.set('time', '08:30');
+
+  // S, M, T, W, T, F, S
+  if (!settings.has('days')) settings.set('days', [false, true, true, true, true, true, false]);
 }
 
 initSettings();
